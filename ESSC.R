9f0180e61f70738949b049327cf4b16ee7f8401d
jdwilson1212 <jdwilson1212@gmail.com>
2015-04-09 16:27:38 -0400

    Added the most updated versions of both codes

--- ESSC.R
+++ ESSC.R
@@ -1,140 +1,110 @@
 ##Extraction of Statistically Significant Communities (ESSC)
-##Written by James David Wilson, 9/6/2013
+##Original Code by James David Wilson; modified code by John Palowich
 ##Function for extracting statistically significant communities in undirected networks
 ##For more information, see the paper "A Testing Based Extraction Algorithm for Identifying 
 ##Significant Communities in Networks", Wilson, et al. 2014 in the Annals of Applied Statistics
 
+symdiff = function(X,Y){setdiff(union(X,Y),intersect(X,Y))}
 
 ###ESSC Function. Run this function to extract communities
-ESSC = function(Adj.Matrix,alpha, Fast = TRUE){
-	degrees = rowSums(Adj.Matrix)
-	n = length(degrees)
-	index = 1:dim(Adj.Matrix)[1]
-	#initialize
-	Community = list()
-	p.values = 1
-	#Running Greedy Search
-	if(Fast == TRUE){
-	#Run Loop across entire network starting at highest degree vertex on each iteration
-	
-	#Begin with community of largest degree
-	vertex.max.degree = which.max(degrees)[1]
-	B0 = c(vertex.max.degree,which(Adj.Matrix[,vertex.max.degree]>0))
-	i = 0
-	#Run First Extraction loop
-		i = i+1
-		temp = Main.Search(Adj.Matrix,alpha,B0) #run main search
-		#Update
-		#index = temp$index
-		Community[[i]] = temp$Community
-		index = setdiff(index,temp$Community)
-		
-		#Update B0 and the vertex of max degree
-		temp.adj = Adj.Matrix[index,index]
-		temp.degs = degrees[index]
-		new.max = which.max(temp.degs)
-		vertex.max.degree = index[new.max]
-		B0 = index[which(temp.adj[,new.max]>0)]
-		print(i)
-		#Function to check the overlap of the communities
-		match.check = function(x){
-			match = rep(0,(length(x)-1))
-			for(i in 1:(length(x)-1)){
-				match[i] = length(intersect(x[[length(x)]],x[[i]]))/length(x[[length(x)]])
-			}
-			max.match = max(match)
-			return(max.match)
-		}
-	j = 0
-	while(length(Community[[i]]) > 0 & length(index) > 0 & j < 30){
-        j = j + 1
-				i = i+1
-				temp = Main.Search(Adj.Matrix,alpha,B0) #run main search
-				if(length(temp$Community) == 0 | length(temp$Community) == n ){
-					break}
-					max.match = match.check(list(Community,temp$Community))
-				if(max.match > 0.75){break}
-				#Update
-				index = setdiff(index,temp$Community)
-				Community[[i]] = temp$Community
+ESSC = function(Adj.Matrix,alpha, Null = "Binomial", Num.Samples = nrow(Adj.Matrix)){
+    degrees = rowSums(Adj.Matrix)
+    n = length(degrees)
+    index = 1:dim(Adj.Matrix)[1]
+    extractFrom = sample(index,Num.Samples,replace = FALSE)
+    #initialize
+    Community = rep(list(NULL),Num.Samples)
+    unq = TRUE
+    nodes = matrix(0,2,Num.Samples)
+    nodes[1,] = extractFrom
+    which_match = 0
+
+    
+    i = 1
+    count = 0
+    #Running long loop
+    for(node in extractFrom){
+        count = count+1
+        cat("######################################################\n")
+        cat(paste0("extraction ",count,"\n"))
+        B0 = c(node,which(Adj.Matrix[node,]>0))
+        temp = Main.Search(Adj.Matrix, alpha, B0, Null)
         
-				if(length(index) == 1){break}
-				#Update B0 and the vertex of max degree
-				temp.adj = Adj.Matrix[index,index]
-				temp.degs = degrees[index]
-				new.max = which.max(temp.degs)
-				vertex.max.degree = index[new.max]
-				#B0 = index[which(temp.adj[,new.max]>0)]
-				B0 = which(Adj.Matrix[,vertex.max.degree]>0)
-			}
-	#Keep only unique communities
-	Community = unique(Community)
-	}
-	
-	
-	#Run Search across all neighborhoods
-	#Here, we use a parallel for loop to run quickly (automatically uses available cores)
-	if(Fast == FALSE){
-		Results = foreach(i = 1:n, .export = c("Main.Search")) %dopar% 
-		    Main.Search(Adj.Matrix,alpha,which(Adj.Matrix[,i]>0))
-		
-		#Keep only unique communities
-		Results = unique(Results)
-		k = length(Results)
-		for(i in 1:k){
-			Community[[i]] = Results[[i]]$Community
-			Final.p.values[[i]] = Results[[i]]$Final.p.values
-			Mean.p.value[i] = Results[[i]]$Mean.p.value
-		}
-		all.vertices.sign = unlist(Community)
-		all.vertices.sign = all.vertices.sign[unique(all.vertices.sign)]
-		}
-
-		
-v = length(Community)
-Background = setdiff(1:n, matrix(unlist(Community[1:v])))
-return(list(Communities = Community[1:v], Background = Background))
+        if(length(temp$Community)>1){
+            if(i>1){
+                unq = TRUE
+                which_match = 0
+                for(j in 1:i){
+                    if(length(symdiff(Community[[j]],temp$Community))==0){
+                        unq = FALSE
+                        which_match = j
+                    }
+                }
+            }
+            if(unq){
+                Community[[i]] = temp$Community
+                nodes[2,count] = i
+                i = i+1
+            } else{
+                nodes[2,count] = which_match
+            }
+        } else{
+            nodes[2,count] = 0
+        }
+    }
+    
+    Community = Community[unlist(lapply(Community,function(comm)length(comm)>0))]
+    Background = setdiff(1:n,unlist(Community))
+    
+    if(length(Community) == 0)
+        Community = list(NULL)
+    
+    return(list(Communities = Community,Background = Background,nodes = nodes))
 }
-
 
 ###Main Search Function
 
-Main.Search = function(Adj.Matrix,alpha,B0){
-  j = 0
-  
-  degrees = rowSums(Adj.Matrix)
-  n = length(degrees)
-  B1 = B0 - 1 #Arbitrary, ensuring B1 and B0 are not equal
-  #Function for checking equality of matrices 
-  vectorequal = function(x,y){
-    is.numeric(x) && is.numeric(y) && length(x) == length(y) && all(x==y)
-  }
-  while(vectorequal(B0,B1) == FALSE & j < 200){
-    j = j+1
-    if(j > 1){B0 = B1}
-    if(length(B0)<2){num.edges.incident = Adj.Matrix[B0,]} 
-    if(length(B0) >=2){
-      num.edges.incident = colSums(Adj.Matrix[B0,])
+Main.Search = function(Adj.Matrix,alpha,B0, Null){
+    j = 0
+    degrees = rowSums(Adj.Matrix)
+    n = length(degrees)
+    #Ensuring B1 and B0 are not equal to start the loop
+    B1 = integer(0)
+    #Function for checking equality of matrices 
+    vectorequal = function(x,y){
+        is.numeric(x) && is.numeric(y) && length(x) == length(y) && all(x==y)
     }
-    
-    prob.B = sum(degrees[B0])/sum(degrees) #probability of connection to B
-    
-    #Handle vertices that are not connected to the remainder of the graph
-    #Modify p-values of these to be 1 since they are not connected
-    indx = which(degrees == 0)
-    p.values = pbinom(num.edges.incident,degrees,prob.B,lower.tail = FALSE)
-    p.values[indx] = 1 
-    
-    #Adjust the p-values according to Benjamini-Hochberg procedure
-    adj.p.values = p.adjust(p.values,method="BH")
-    B1 = which(adj.p.values < alpha)
-    Mean.p.value = mean(adj.p.values[B1])
-    if(length(B1) == 0){break}
-    if(is.na(vectorequal(B0,B1))==TRUE){
-      Mean.p.value = mean(p.values[B1])
-      break
+    cat("###### Starting Main.Search Loop ######\n")
+    while(vectorequal(B0,B1) == FALSE & j <= 30){
+        
+        j = j+1
+        if(j<5||j%%5==0)
+            cat(paste("iteration",j,"\n"))
+        if(j>1)
+            B0 = B1
+	  if(length(B0)>1)
+	        duBs = rowSums(Adj.Matrix[,B0])        
+	  if(length(B0)==1)
+		  duBs = Adj.Matrix[,B0]
+        pB = sum(degrees[B0])/sum(degrees) #probability of connection to B
+        if(Null == "Binomial")
+            pvals = pbinom(duBs,degrees,pB,lower.tail = FALSE)
+        if(Null == "Poisson")
+            pvals = ppois(duBs,degrees*pB,lower.tail = FALSE)
+        pvals[degrees==0] = 1
+        #cat(paste0("length pvals is ",length(pvals),"; mean is ",round(mean(pvals),2),"\n"))
+        pvals_bh = pvals*n/rank(pvals)
+        if(sum(pvals_bh<=alpha)==0){B1 = integer(0);cat("B1 reached size 0 in Main.Search\n");break}
+        threshold = max(pvals[pvals_bh<=alpha])
+        B1 = which(pvals<=threshold)
+        if(length(B1)<1){break}
     }
-  }
-  Community = B1
-  return(list(Community = Community))
+    Community = B1
+    if(j>30){
+        cat("Main.Search failed to converge before 30 iterations\n")
+        Community = integer(0)
+    }
+    return(list(Community = Community))
 }
+
+
---
+++ stochastic_block_model.R
@@ -0,0 +1,57 @@
+#Source for the Stochastic Block Model
+#Written by James D. Wilson; University of North Carolina at Chapel Hill
+require(Rlab)
+require(Matrix)
+stochastic.block = function(n, k = 2, p.diff, p.between, ind = c(0, floor(n/2), n), background = FALSE, bn = 0){
+  ##Generate a Stochastic block model
+  #Function will create a realization of the stochastic block model where each block does not overlap. 
+  #Here, the probability of an edge between blocks is given by p and the probability of connection within blocks is given by r. 
+  #Input: 
+  #   k = number of blocks (default = 2)
+  #   p.diff = additional probability of connection within blocks (p.within - p.between)
+  #   p.between = probability of connection between blocks
+  #   ind = index of which communities belong to which community
+  #         ex: ind = c(0, 100, 200) contains two blocks: from 1 to 100 and from 101 to 200
+  #         default = c(0, floor(n/2), n) gives a stochastic 2 block model with equal sized blocks
+  #   Requires the use of "Rlab" and "Matrix"
+  Y = matrix(rep(0,n*k),ncol = k) 
+  for(i in 1:k){
+    Y[(ind[i]+1):ind[i+1],i] = 1
+  }
+  B = diag(rep(1,k))*p.diff + p.between * rep(1,k) %*% t(rep(1,k))
+  if(background == TRUE){
+    M = matrix(rep(bn,(k+1)^2),ncol = k+1)
+    M[1:k,1:k] = B
+    B = M
+    indx = which(rowSums(Y) < 1)
+    L = rep(0,n)
+    L[indx] = 1
+    Y = cbind(Y,L)
+  } 
+  #ind = c(0,cumsum(size))
+  #Left Sender community
+  
+  #Z = 1 - Y
+  expected.A = Y%*%B%*%t(Y)/2
+  Adj = matrix(rbern(n^2, matrix(expected.A, ncol = 1)), ncol = n)
+  #Make this symmetric
+  Adj = Adj + t(Adj)
+  Adj[which(Adj > 0)] = 1
+  diag(Adj) = 0
+  Adj = Matrix(Adj, sparse = TRUE)
+  return(Adj)
+}
+
+## Examples:
+
+# Example 1: a block model with 3 blocks, 1000 nodes, inner block probability = 0.10, between block probability = 0.05
+
+Adjacency <- stochastic.block(1000, 3, 0.05, 0.05, ind = c(0,300, 600, 1000))
+
+image(Adjacency)
+
+# Example 2: a block model with 3 blocks as above but with background vertices belonging to no community.
+# Here, background vertices are connected at random with probability bn = 0.05
+Adjacency.background <- stochastic.block(1000, 3, 0.05, 0.05, ind = c(0,300, 600, 800), background = TRUE, bn = 0.05)
+
+image(Adjacency.background)
